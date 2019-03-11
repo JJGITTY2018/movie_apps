d01520e5444ed23f6ef2c7e43df16a2b5ae305f1
JJ <jackyong@pursuit.org>
2019-03-11 17:44:56 -0400

    file move

---
+++ movies_frontend/src/2_body/home.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+
+
+class Home extends Component {
+
+
+  render(){
+    return (
+      <>
+      <h1> Welcome to Movie App </h1>
+      </>
+    )
+  }
+
+
+}
+
+
+export default Home
