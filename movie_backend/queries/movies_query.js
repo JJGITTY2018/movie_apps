33b621fbf13f6329c3fdd0333906939812544e8e
JJ <jackyong@pursuit.org>
2019-03-13 10:23:15 -0400

    personal page

--- movie_backend/queries/movies_query.js
+++ movie_backend/queries/movies_query.js
@@ -3,7 +3,7 @@ const {
 } = require("../db/db.js")
 
 const getAllMovie = (req, res, next) => {
-  db.any('SELECT movies.id, img_url, title, name AS genre_name, genre.id AS genre_id, AVG(rating.stars) AS Rating FROM movies JOIN genre ON movies.genre_id = genre.id JOIN rating ON movies.id = rating.id GROUP BY movies.id, img_url, title, name, genre.id').then((data)=>{
+  db.any('SELECT movies.id, img_url, title, name AS genre_name, genre.id AS genre_id, AVG(rating.stars) AS Average_Rating FROM movies JOIN genre ON movies.genre_id = genre.id JOIN rating ON movies.id = rating.id GROUP BY movies.id, img_url, title, name, genre.id').then((data)=>{
     res.status(200).json({
       status:200,
       data:data
@@ -17,7 +17,8 @@ const getAllMovie = (req, res, next) => {
 }
 
 const getSingleInfo = (req, res, next) => {
-  db.any('SELECT movies.id, img_url, title, name AS genre_name, genre.id AS genre_id, AVG(rating.stars) AS Rating FROM movies JOIN genre ON movies.genre_id = genre.id JOIN rating ON movies.id = rating.id WHERE ID = $1 GROUP BY movies.id, img_url, title, name, genre.id',{params:req.params.id}).then((data) => {
+  console.log(req.params.id)
+  db.any('SELECT movies.id, img_url, title, name AS genre_name, genre.id AS genre_id, AVG(rating.stars) AS Rating FROM movies JOIN genre ON movies.genre_id = genre.id JOIN rating ON movies.id = rating.id WHERE movies.id = $1 GROUP BY movies.id, img_url, title, name, genre.id',[req.params.id]).then((data) => {
     res.status(200).json({
       status: 200,
       data: data
@@ -31,7 +32,7 @@ const getSingleInfo = (req, res, next) => {
 }
 
 const getCommentsByMovieID = (req, res, next) => {
-  db.any('SELECT * FROM Comments WHERE  movies_id = $1', { params: req.params.id }).then((data) => {
+  db.any('SELECT * FROM Comments WHERE  movies_id = $1', [req.params.id]).then((data) => {
     res.status(200).json({
       status: 200,
       data: data
@@ -46,16 +47,14 @@ const getCommentsByMovieID = (req, res, next) => {
 
 
 const insertComment = (req, res, next) => {
-  db.any("INSERT INTO comments (comments, movies_id) VALUES($1, $2)" , {
-    comments: req.body.comments,
-    movie_id: req.body.movie_id
-  }).then((data) => {
+  db.any("INSERT INTO comments (comments, movies_id) VALUES($1, $2)" , [req.body.comments,req.body.movie_id]
+  ).then((data) => {
     res.status(200).json({
       status: 200,
       message: "success"
 
     })
-  }).catch(err => {
+  }).catch(err => {cx
     res.json({
       status: 404,
       message: err.message
@@ -80,6 +79,35 @@ const insertRating = (req, res, next) => {
   })
 }
 
-module.exports = { getAllMovie, getSingleInfo, getCommentsByMovieID, insertRating}
+const getMovieByTitle = (req, res, next) => {
+  let params = "%"+req.params.id+"%"
+  db.any('SELECT movies.id, img_url, title, name AS genre_name, genre.id AS genre_id, AVG(rating.stars) AS Average_Rating FROM movies JOIN genre ON movies.genre_id = genre.id JOIN rating ON movies.id = rating.id WHERE LOWER(movies.title) LIKE $1 GROUP BY movies.id, img_url, title, name, genre.id',params).then((data) => {
+    res.status(200).json({
+      status: 200,
+      data: data
+    })
+  }).catch(err => {
+    res.status(400).json({
+      status: 404,
+      err: err
+    })
+  })
+}
+
+
+const getGenreList = (req, res, next) => {
+  db.any('SELECT name AS genre_name, genre.id AS genre_id FROM genre').then((data) => {
+    res.status(200).json({
+      status: 200,
+      data: data
+    })
+  }).catch(err => {
+    res.status(400).json({
+      status: 404,
+      err: err
+    })
+  })
+}
+module.exports = { getAllMovie, getSingleInfo, getCommentsByMovieID, insertRating, insertComment, getMovieByTitle, getGenreList}
 
 
--- movie_backend/routes/movies_routes.js
+++ movie_backend/routes/movies_routes.js
@@ -1,14 +1,18 @@
-import {db} from "../db/db.js"
 
 const express = require('express')
 const router = express.Router();
 
-const { getAllMovie, getSingleInfo, getCommentsByMovieID, insertRating }= require("../queries/movies_query.js")
+const { getAllMovie, getSingleInfo, getCommentsByMovieID, insertRating, insertComment, getMovieByTitle, getGenreList}= require("../queries/movies_query.js")
 
-/* GET users listing. */
-router.get('/:id', getSingleInfo )
+/* GET STUFF. */
+router.get('/genre',getGenreList)
+router.get('/search/:id', getMovieByTitle)
+router.get('/:id/comments/', getCommentsByMovieID)
+router.get('/:id', getSingleInfo)
+
 router.get('/', getAllMovie)
-router.post('/follow', insertFollow)
+//POSTING STUFF
+router.post('/rating', insertRating)
 router.post('/comment', insertComment)
 
 
